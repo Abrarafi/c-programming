3c813a17fe3889d02f67208554921409c7c56b47
Abrarafi <106591751+Abrarafi@users.noreply.github.com>
2022-08-05 17:02:58 +0600

    Update D_The_Clock.cpp

--- D_The_Clock.cpp
+++ D_The_Clock.cpp
@@ -2,7 +2,6 @@
 using namespace std;
 #define vi vector<int>
 #define pi pair<int,int>
-#define f first
 #define sc second
 #define ll long long
 #define TestCase int testcase, cs=1; cin>>testcase;while(testcase--)
