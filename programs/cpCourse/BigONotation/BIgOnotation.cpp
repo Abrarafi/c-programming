a95624a54520070240da4dee4f392f7387f88f02
Abrarafi <106591751+Abrarafi@users.noreply.github.com>
2022-08-05 16:33:37 +0600

    Initial commit

---
+++ programs/cpCourse/BigONotation/BIgOnotation.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define vi vector<int>
+#define pi pair<int,int>
+#define f first
+#define sc second
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    return 0;
+}
