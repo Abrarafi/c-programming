f573b18003d51eba72a7e20f4bd1279374f45899
Abrarafi <106591751+Abrarafi@users.noreply.github.com>
2022-08-30 00:19:52 +0600

    with bitmasking

--- B_Fedor_and_New_Game.cpp
+++ B_Fedor_and_New_Game.cpp
@@ -9,17 +9,13 @@ int main()
     {
         cin>>v[i];
     }
-    int test=0;
     int count=0;
-    string s= bitset<32>(v[m]).to_string();
+    int test=0;
     for(int i=0;i<m;i++)
     {
-        string st=bitset<32>(v[i]).to_string();
-        //cout<<st<<" ";
-        for(int j=0;j<32;j++)
+        for(int j=0;j<n;j++)
         {
-            if(s[j]!=st[j])test++;
-            
+            if((v[m]&(1<<j))!=(v[i]&(1<<j))) test++;
         }
         if(test<=k)count++;
         test=0;
