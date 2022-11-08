b4943b1f0d3ee79178d8bd2f0b0bbc933b6029c2
Abrarafi <106591751+Abrarafi@users.noreply.github.com>
2022-11-08 12:41:50 +0600

    solved

--- programs/begining.cpp
+++ programs/begining.cpp
@@ -1,12 +1,6 @@
-#include <iostream>
-#include <string>
+#include<bits/stdc++.h>
 using namespace std;
-
 int main()
 {
-string bin_string = "10101010";
-int number = 0;
-number = stoi(bin_string, nullptr, 2);
-cout <<"Original binary string: "<< bin_string << endl;
-cout <<"Equivalent integer: "<< number << endl;
+    cout<<"hello world"<<endl;
 }
